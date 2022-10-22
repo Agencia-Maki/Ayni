799cb73e4195f799377c8ade44aef6e7c27a29d6
Jorge Luis <jluis.developer@gmail.com>
2022-10-22 11:55:11 -0500

    fix sidebar for macs

--- AyniFrontend/src/components/AppSidebar.js
+++ AyniFrontend/src/components/AppSidebar.js
@@ -18,14 +18,14 @@ import navigation from '../_nav'
 const AppSidebar = () => {
   const dispatch = useDispatch()
   const unfoldable = useSelector((state) => state.sidebarUnfoldable)
-  const sidebarShow = useSelector((state) => state.sidebarShow)
+  // const sidebarShow = useSelector((state) => state.sidebarShow)
 
   return (
     <CSidebar
       className="bg-dark-gradient"
       position="fixed"
       unfoldable={unfoldable}
-      visible={sidebarShow}
+      visible={true}
       onVisibleChange={(visible) => {
         dispatch({ type: 'set', sidebarShow: visible })
       }}
